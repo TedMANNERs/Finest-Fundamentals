5cb5d049f704d6913f686779241cdd4d9e0645db
BinaryTENSHi <binarytenshi@gmail.com>
2013-09-28 15:57:54 +0200

    added ant build script implemented fingerprint checking

--- common/com/binarytenshi/fundamentals/Fundamentals.java
+++ common/com/binarytenshi/fundamentals/Fundamentals.java
@@ -23,6 +23,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLFingerprintViolationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -32,15 +33,13 @@ import cpw.mods.fml.common.network.NetworkMod;
  * Main mod class for Finest-Fundamentals.
  * 
  * @author BinaryTENSHi
- *
  */
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:IC2")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:IC2", certificateFingerprint = Reference.FINGERPRINT)
 @NetworkMod(channels = Reference.CHANNEL_NAME, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class Fundamentals {
-
     @Instance(Reference.MOD_ID)
     public static Fundamentals instance;
-    
+
     public static Logger logger = Logger.getLogger(Reference.MOD_NAME);
 
     public static CreativeTabs creativeTab = new CreativeTabs(Reference.MOD_ID);
@@ -48,11 +47,13 @@ public class Fundamentals {
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
 
+    static {
+        logger.setParent(FMLLog.getLogger());
+    }
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         ConfigHandler.init(event.getSuggestedConfigurationFile());
-
-        logger.setParent(FMLLog.getLogger());
 
         ModItems.init();
         ModBlocks.init();
@@ -74,5 +75,14 @@ public class Fundamentals {
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         GuiContainerManager.tooltipHandlers.add(new ContentTooltipHandler());
+    }
+
+    @EventHandler
+    public void invalidFingerprint(FMLFingerprintViolationEvent event) {
+        if (Reference.FINGERPRINT.equals("@FINGERPRINT@")) {
+            logger.warning(Strings.NO_FINGERPRINT_MESSAGE);
+        } else {
+            logger.severe(Strings.INVALID_FINGERPRINT_MESSAGE);
+        }
     }
 }
--- common/com/binarytenshi/fundamentals/lib/Strings.java
+++ common/com/binarytenshi/fundamentals/lib/Strings.java
@@ -12,4 +12,7 @@ public class Strings {
     public static final String RESOURCE_PREFIX = Reference.MOD_ID + ":";
 
     public static final String NBT_CONTENT = "content";
+    
+    public static final String NO_FINGERPRINT_MESSAGE = "This version of Finest Fundamentals is a development version of the mod. It may be unstable and/or incomplete.";
+    public static final String INVALID_FINGERPRINT_MESSAGE = "This version of Finest Fundamentals has been modified. Please consider downloading the original version.";
 }
